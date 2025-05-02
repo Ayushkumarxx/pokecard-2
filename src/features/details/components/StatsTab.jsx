e0fa3bb2b82a89ee6ff5b9f4e85b5623d6d25bc4
ayush kumar <ayushkumar09005@gmail.com>
2025-05-02 17:18:09 +0530

    changes

--- src/features/details/components/StatsTab.jsx
+++ src/features/details/components/StatsTab.jsx
@@ -1,5 +1,5 @@
 const StatsTab = ({ pokemon }) => (
-  <div className="px-4 sm:px-2">
+  <div className="px-2 sm:px-6">
     <h2 className="text-2xl font-bold mb-10 text-center text-white">Base Stats</h2>
 
     <div className="max-w-3xl mx-auto space-y-8">
