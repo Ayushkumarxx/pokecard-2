56cded15b36d23a8a4adda6056a44922c2d49965
ayush kumar <ayushkumar09005@gmail.com>
2025-05-02 17:28:01 +0530

    changes

--- src/shared/components/SearchBar.jsx
+++ src/shared/components/SearchBar.jsx
@@ -55,7 +55,7 @@ function SearchBar({ onSearch }) {
           type="text"
           value={query}
           onChange={handleInputChange}
-          placeholder="Search Pokémon"
+          placeholder="Search"
           className="rounded-full pl-12 pr-10 py-3 bg-[#333333] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#FFCB05] w-48 md:w-64 transition-all duration-300 font-semibold"
         />
         <FiSearch className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 text-xl" />
